0b336575f251586f48ec25b11c1738de48f08c14
Nenad <kuzic_nenad@protonmail.com>
2025-03-26 22:10:38 +0100

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,16 @@
+// Currently empty, but here's a template for future utility functions.
+// You can add input validation, trimming, string formatting, etc.
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Example utility function to trim newline characters
+void trim_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
+// Add more utilities here as needed
